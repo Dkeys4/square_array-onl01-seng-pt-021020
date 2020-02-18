e6856b2b0d4cc9caee98c238055ce912bacf14fd
Dural Keys <duralinitaly@gmail.com>
2020-02-18 05:28:47 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  
+  nums = []
+ 
+ numbers.each do |digit| 
+   digit**2 
+  ##code happens after pipes
+  
+  nums << digit**2
+  
+  
+  end
+  
+    nums 
 end
